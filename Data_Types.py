21e8b51370992f26b50bcbb1a3395125d791dbec
jiyapatel25032006-creator <jiyapatel25032006@gmail.com>
2026-07-17 23:53:33 +0530

    Practice Data

---
+++ Data_Types.py
@@ -0,0 +1,100 @@
+#  Integer (int)//
+# age = 22
+# marks = 95
+# print(type(age))
+# print(type(marks))
+
+
+#Operations on Integers//
+# a = 10
+# b = 3
+
+# print(a + b)   # Addition
+# print(a - b)   # Subtraction
+# print(a * b)   # Multiplication
+# print(a / b)   # Division
+# print(a % b)   # Modulus
+
+# Float (float)//
+# price = 199.99
+# temperature = 36.7
+
+# print(type(price))
+
+# String (str)//
+# name = "Python"
+# city = 'Ahmedabad'
+
+# print(name)
+# print(city)
+
+# String Indexing//
+# word = "Python"
+# print(word[0])
+# print(word[3])
+
+# Negative Indexing//
+# word = "Python"
+# print(word[-1])
+
+# String Methods//   
+#upper//     #syntax:object.method
+# name = "python"         
+# print(name.upper())
+
+#lower//
+# name = "python"
+# print(name.lower())
+
+#replace//
+# text = "I like Java"
+# print(text.replace("Java", "Python"))
+
+#boolean(bool)//
+# is_student = True
+# print(type(is_student))
+
+# Comparison Operators//
+# age = 20
+# print(age >= 18)
+
+# List (list)//                 #List stores multiple values//
+fruits = ["Apple", "Banana", "Mango"]                 #Accessing List Items//
+# print(fruits[0])
+# print(fruits[1])
+
+# fruits[1] = "Orange"              #Modifying list
+# print(fruits)
+
+#  Important List Methods//
+#append() //    #Adds item at end//
+# fruits.append("Grapes")
+
+#remove//
+# fruits.remove("Apple")
+
+#pop// #removes item using index
+# fruits.pop(0)
+
+#Tuple (tuple) //          # Tuple is similar to list but immutable.
+# colors = ("Red", "Green", "Blue")
+# print(colors)
+
+# Set (set)//         # Set stores unique values only//
+# numbers = {1,2,3,3,4}
+# print(numbers)
+
+# numbers.add(10)               #add//
+# numbers.remove(2)             #remove//
+
+# Dictionary (dict)         # Dictionary stores data in key-value pairs
+# student = {
+#     "name": "Rahul",
+#     "age": 21,
+#     "city": "Delhi"
+# }
+# print(student["name"])               # Access Dictionary Values//
+# student["marks"] = 95                 #Add New Data//
+# student["age"] = 22                   #Update Existing Data//
+
+
