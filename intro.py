e2a24a2acc2d6508c59d627229d98c2686ce5a88
jiyapatel25032006-creator <jiyapatel25032006@gmail.com>
2026-07-17 16:22:02 +0530

    Practice

---
+++ intro.py
@@ -0,0 +1,60 @@
+# print() Function//
+
+# print("JIYA")
+# print("25")
+# print("Welcome to Data Analytics")
+# print("Age:", 25)  # print("Age: 25")
+
+#print Multiple Values//
+# name = "Rohan"
+# age = 25
+# print("Name:", name, "Age:", age)
+
+# Separator and End//
+# print("Hello", "World", sep="-")
+# print("Hello", end=" ")
+# print("World")
+
+# input() Function//    #input() always returns data as string//
+# name = input("Enter your name: ")
+# print("Welcome", name)
+
+# Converting Input//
+# age = int(input("Enter age: "))
+# salary = float(input("Enter salary: "))
+# print("Age:",age,"Salary:",salary)
+
+# Multiple Assignment//
+# a, b, c = 10, 20, 30
+# print(a, b, c)
+
+#Dynamic Typing//
+# x = 10
+# x = "Hello"
+# print(x)
+
+# Checking Data Type//
+# x = 10
+# print(type(x))
+
+
+# Operators in Python//
+# a = 10
+# b = 5
+# print("Addition:", a + b)
+# print("Multiplication:", a * b)
+
+#PRACTICE//
+# name = input("Enter name: ")
+# age = int(input("Enter age: "))
+# print("Name:", name, "Age:", age)
+
+# name = input("What is your name?")
+# age = int(input("What is your age?"))
+# print("My name is",name,"and my age is",age)
+# print("My name is",name,"and I am",age,"years old")
+
+# Name=input("My name is:")
+# Course=input("My course is:")
+# Fees=int(input("My fees is:"))
+# print("My name is",Name,"and my course is",Course,"and my fees is",Fees)
