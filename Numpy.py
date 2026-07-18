01e0f1fd15e241d2526579b521dd42f17ed61299
jiyapatel25032006-creator <jiyapatel25032006@gmail.com>
2026-07-18 20:47:08 +0530

    Practice Data

---
+++ Numpy.py
@@ -0,0 +1,23 @@
+# import numpy as np
+# salary = np.array([20000, 30000, 40000])
+# print(salary * 1.10)
+
+# import numpy as np
+# arr = np.array([10,20,30])
+# print(arr + arr)
+
+# import numpy as npṇ
+# marks = np.array([85, 90, 78, 92])
+# print(marks)
+
+# sales = np.array([12000, 15000, 18000, 20000])
+
+# print(sales)
+
+employee_data = np.array([
+    [101, 25000],
+    [102, 30000],
+    [103, 35000]
+])
+
+print(employee_data)
